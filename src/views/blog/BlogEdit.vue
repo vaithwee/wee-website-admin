87995a2a10a54f391f4c93d67205247871b3e111
Vaith <vaithwee@yeah.net>
2020-02-18 23:09:47 +0800

    Article edit add get tags and categories

--- src/views/blog/BlogEdit.vue
+++ src/views/blog/BlogEdit.vue
@@ -1,12 +1,51 @@
 <template>
     <div>
-        Edit
+        <el-form v-model="form" label-width="80px">
+            <el-form-item label="标题" >
+                <el-input v-model="form.name" ></el-input>
+            </el-form-item>
+            <el-form-item label="分类" >
+                <el-select v-model="form.type" placeholder="请选择分类" >
+                    <el-option v-for="c in categories" :label="c.name" :value="c.id" :key="c.id">
+
+                    </el-option>
+                </el-select>
+            </el-form-item>
+            <el-form-item label="标签">
+                <el-checkbox-group v-model="form.tags">
+                    <el-checkbox :label="t.name" :value="t.id" name="tags" v-for="t in tags" :key="t.id"></el-checkbox>
+                </el-checkbox-group>
+            </el-form-item>
+        </el-form>
     </div>
 </template>
 
 <script>
+    import {getCategoryList} from "@/network/category";
+    import {getTagList} from "@/network/tag";
+
     export default {
-        name: "BlogEdit"
+        name: "BlogEdit",
+        data() {
+            return {
+                form: {
+                    name: '',
+                    type: null,
+                    tags:[]
+                },
+                categories: [],
+                tags: [],
+            }
+        },
+        created() {
+            getCategoryList(0, 200).then(res => {
+                this.categories = res.data;
+            });
+
+            getTagList(0, 200).then(res => {
+                this.tags = res.data;
+            })
+        }
     }
 </script>
 
