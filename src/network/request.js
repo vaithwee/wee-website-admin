6a29cf72095ae2103c7c3cc78e3e4ba72fc66ffa
Vaith <vaithwee@yeah.net>
2020-03-03 15:42:57 +0800

    Added request sign and debug mode

--- src/network/request.js
+++ src/network/request.js
@@ -2,29 +2,31 @@ import Axios from "axios";
 import cryptoJS from "crypto-js";
 
 export function request(config) {
+
+    let info = {
+        "en": 0,
+        "appid": 1
+    };
+    let sign = cryptoJS.MD5(encrypt(info).toString()).toString();
+
     const instance = Axios.create({
         baseURL: 'http://localhost:9088',
         timeout: 60000,
         transformRequest: [function (data) {
-            console.log(data);
-            let key = "h2uh123haj89wjoj";
-            let str = JSON.stringify(data);
-            key = cryptoJS.enc.Utf8.parse(key); //秘钥
-            str = cryptoJS.enc.Utf8.parse(str);
-            console.log(str);
-            let result = cryptoJS.AES.encrypt(str, key, {
-                mode: cryptoJS.mode.ECB,
-                padding: cryptoJS.pad.Pkcs7
-            });
-            console.log(result);
-            return result;
-            // return "SAKyhQahrGQPszDWnouZyCqs5Sjjamy73gfleaenNN10OI9WVGOcSsqd+q9nfAeu";
+            console.log("data" + data);
+            if (info.en === 0) {
+                return JSON.stringify(data);
+            }
+            return encrypt(data);
         }],
         headers: {
-            'Content-Type': 'application/json;charset=UTF-8'
+            'Content-Type': 'application/json;charset=UTF-8',
+            'info': JSON.stringify(info),
+            sign
         }
     });
     instance.interceptors.request.use(config => {
+        console.log(config);
         return config;
     }, error => {
         console.log(error);
@@ -38,4 +40,15 @@ export function request(config) {
 
 
     return instance(config);
+}
+
+function encrypt(data) {
+    let key = "h2uh123haj89wjoj";
+    let str = JSON.stringify(data);
+    key = cryptoJS.enc.Utf8.parse(key); //秘钥
+    str = cryptoJS.enc.Utf8.parse(str);
+    return cryptoJS.AES.encrypt(str, key, {
+        mode: cryptoJS.mode.ECB,
+        padding: cryptoJS.pad.Pkcs7
+    });
 }
